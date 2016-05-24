b13dd3a3a34de7c8711b40ebf1fcfdc3ee46e412
Leandro <juxmac@juxmacpro.dynamic.ucsd.edu>
2016-05-24 13:26:11 -0700

    Initial Commit

---
+++ xBeacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xBeacon
+//
+//  Created by Leandro on 5/24/16.
+//  Copyright © 2016 BaDaSS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
